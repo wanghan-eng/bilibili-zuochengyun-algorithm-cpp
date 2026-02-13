df493356a9d78933b07746d0add97c6e8e68b6ff
WangHan <xkrj_0930@QQ.com>
2026-02-13 11:39:47 +0800

    Update PartList.cpp

--- 第12节课--链表划分/PartList.cpp
+++ 第12节课--链表划分/PartList.cpp
@@ -1,7 +1,13 @@
-// 给你一个链表的头节点 head 和一个特定值 x
-// 请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
-// 你应当 保留 两个分区中每个节点的初始相对位置
-// 测试链接 : https://leetcode.cn/problems/partition-list/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
@@ -9,8 +15,8 @@ public:
         ListNode* RH = nullptr, * RT = nullptr;
         ListNode* next = nullptr;
         while (head != nullptr) {
-            next = head->next;//保存下一个结点
-            head->next = nullptr;//断开连接
+            next = head->next;    // 保存下一个结点
+            head->next = nullptr; // 断开连接,必须要断连，否则会有成环的风险
             if (head->val < x) {
                 if (LH == nullptr) {
                     LH = head;
@@ -33,11 +39,10 @@ public:
             }
             head = next;
         }
-        if (LH == nullptr) {//如果左面没有元素，返回右节点
+        if (LH == nullptr) { // 如果左面没有元素，返回右节点
             return RH;
         }
         LT->next = RH;
         return LH;
-
     }
 };
